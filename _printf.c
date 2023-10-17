b3d8942097cd8a0f45c9be3526fc5943a4e490f1
solojobbs <solojobbs@gmail.com>
2023-10-17 08:21:26 -0700

    -printf

--- _printf.c
+++ _printf.c
@@ -1,2 +1,24 @@
 #include "main.h"
 
+
+
+
+
+
+
+int _printf(const char *format, ...)
+{
+	int charcount = 0;
+	va_list args;
+
+	if(*format == NULL)
+	{
+		return (-1);
+	}
+
+	va_start(args, format);
+
+	while(*format)
+	{
+	}
+}
