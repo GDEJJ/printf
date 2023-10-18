29a440ac4d7e5e474e6d208ff02b3a9c6fe4c99c
solojobbs <solojobbs@gmail.com>
2023-10-18 03:26:30 -0700

    Customized printf

--- _printf.c
+++ _printf.c
@@ -18,7 +18,7 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	while(*format)
+	while (*format)
 	{
 		if(*format != '%')
 		{
@@ -35,11 +35,33 @@ int _printf(const char *format, ...)
 					char c = va_arg(args, char);
 					write(1, &c, 1);
                         		charcount++;
+					break;
 				case "s":
+					char *stn = va_arg(args, char*);
+					int slen;
+
+					for (slen = 0; stn[slen] != '\0'; slen++)
+						;
+					
+					write(1, stn, slen);
+					charcount += slen;
+					break;
 				case "%":
+					write(1, format, 1);
+					break;
+				case "d":
+					int i = va_arg(args, int);
+					write(1, i, 4);
+					charcount++;
 				default:
 					break;
 			}
 		}
+
+		format++;
 	}
+	
+	va_end(args);
+
+	return charcount;
 }
