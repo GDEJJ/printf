93004783b100e89b2bdabb479a2a1b5f39bf0e4d
GDEJJ <odejayiolamide@gmail.com>
2023-10-18 03:35:19 -0700

    check

--- main.h
+++ main.h
@@ -5,5 +5,15 @@
 #include <unistd.h>
 
 int _printf(const char *format, ...);
+int print_bin(va_list val);
+int printf_char(va_list val);
+int _putchar(char c);
+int printf_string(va_list val);
+int _strlen(char *str);
+int _strlenc(const char *str);
+int print_37(void);
+int print_int(va_list args);
+int print_dec(va_list args);
+int print_unsigned(va_list args);
 
 #endif
